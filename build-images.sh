bb5d17625b1b1e64490ceae09fe4be7394d1b21b
IoriBranford <ioribranford@gmail.com>
2025-07-21 15:17:51 -0700

    Ensure full and ghaction images are based on the local env image

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker build . -f Dockerfile -t ioribranford/build-love-android:11.5-env
-docker build . -f Dockerfile-prebuilt -t ioribranford/build-love-android:11.5-full
-docker build . -f Dockerfile-action -t ioribranford/build-love-android:11.5-ghaction
+docker build . --pull=false -f Dockerfile-prebuilt -t ioribranford/build-love-android:11.5-full
+docker build . --pull=false -f Dockerfile-action -t ioribranford/build-love-android:11.5-ghaction
