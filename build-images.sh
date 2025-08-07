9e6c4a3aa6cd8b5a6cf3044446a3806596aaafa9
IoriBranford <ioribranford@gmail.com>
2025-08-06 17:16:24 -0700

    Enable buildkit

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export DOCKER_BUILDKIT=1
+
 docker build . -f Dockerfile -t ioribranford/build-love-android:11.5-env
 docker build . --pull=false -f Dockerfile-prebuilt -t ioribranford/build-love-android:11.5-full
 docker build . --pull=false -f Dockerfile-action -t ioribranford/build-love-android:11.5-ghaction
